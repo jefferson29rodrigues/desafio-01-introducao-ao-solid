9340737203a51f0e9ec8cfd48779ebdc55be3ea7
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2022-04-07 22:22:05 -0300

    desafio 4/7 ok

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const userIsAdmin = this.usersRepository.findById(user_id);
+
+    if (!userIsAdmin) {
+      throw new Error("User is not admin");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
